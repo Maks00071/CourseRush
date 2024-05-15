8f4e982c32bd422cbfce2bf8105253c804a3f750
Maks Korystov <corystov.maks@yandex.ru>
2024-05-15 23:34:51 +0300

    ADD module1.Level18 ClassPriorityQueue

--- src/Module1/Level18/ClassPriorityQueue.java
+++ src/Module1/Level18/ClassPriorityQueue.java
@@ -1,6 +1,8 @@
 package Module1.Level18;
 import java.util.Comparator;
 import java.util.PriorityQueue;
+import java.util.Deque;
+import java.util.ArrayDeque;
 
 public class ClassPriorityQueue {
     public static void main(String[] args) {
@@ -13,5 +15,41 @@ public class ClassPriorityQueue {
         while (!priorityQueue.isEmpty()) {
             System.out.println(priorityQueue.remove());
         }
+        System.out.println("--------------------------");
+        Deque<String> deque1 = new ArrayDeque<>();
+
+        deque1.add("Apple");
+        deque1.addFirst("Orange");
+        deque1.add("Lemon");
+        deque1.addLast("Pineapple");
+
+        System.out.println(deque1);
+        System.out.println("--------------------------");
+
+        System.out.printf("The first element from deque is %s%n", deque1.getFirst());
+        System.out.printf("The last element from deque is %s%n", deque1.getLast());
+
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
