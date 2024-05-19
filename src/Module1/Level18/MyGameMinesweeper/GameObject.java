ca6e081c60e7246eeb0c936e1b6319cad9301ce3
Maks Korystov <corystov.maks@yandex.ru>
2024-05-19 20:13:56 +0300

    CHANGE module1.Level18 GameObject

--- src/Module1/Level18/MyGameMinesweeper/GameObject.java
+++ src/Module1/Level18/MyGameMinesweeper/GameObject.java
@@ -10,6 +10,13 @@ public class GameObject {
     public boolean isMine; //является ли ячейка миной
     public int countMineNeighbors; //количество "заминированных" соседей ячейки
 
+    /**
+     * Конструктор
+     * @author Korystov M.
+     * @param x - координата x
+     * @param y - координата y
+     * @param isMine - наличие мины в ячейке
+     */
     public GameObject(int x, int y, boolean isMine) {
         this.x = x;
         this.y = y;
