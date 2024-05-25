416b46a3e8ca2f8dde150cb5c448bce16e5249b6
Maks Korystov <corystov.maks@yandex.ru>
2024-05-25 22:13:07 +0300

    ADD module1.Level19 MyFirstSingleton

---
+++ src/Module1/Level19/Tasks/MyFirstSingleton/Solution.java
@@ -0,0 +1,7 @@
+package Module1.Level19.Tasks.MyFirstSingleton;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Экземпляр синглтона: " + MySingleton.INSTANCE);
+    }
+}
