1606d39e74ab8c2d50617adab0f6b8588e0c7be5
Maks Korystov <corystov.maks@yandex.ru>
2024-05-29 20:19:32 +0300

    ADD module1.Level20 CheckedException

---
+++ src/Module1/Level20/CheckedExceptions.java
@@ -0,0 +1,17 @@
+package Module1.Level20;
+
+public class CheckedExceptions {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Программа работает от забора");
+            Thread.sleep(1000);
+            System.out.println("до обеда");
+        } catch (NullPointerException | NumberFormatException e) {
+            System.out.println("Произошло исключение на букву N");
+        } catch (IllegalArgumentException | IllegalStateException | InterruptedException e) {
+            System.out.println("Произошло исключение на букву I");
+        } catch (ClassCastException e) {
+            System.out.println("Произошло исключение на букву C");
+        }
+    }
+}
