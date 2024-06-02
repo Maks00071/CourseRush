b6fd2ce99af877aa882a0fc949ff865c95396860
Maks Korystov <corystov.maks@yandex.ru>
2024-06-02 13:50:57 +0300

    Change new Package "My Games"

--- src/Module1/Level21/CastomesException/Solution.java
+++ src/Module1/Level21/CastomesException/Solution.java
@@ -1,7 +1,36 @@
 package Module1.Level21.CastomesException;
 
 public class Solution {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
+        OurCoolUncheckedException exception = getException(3);
+        System.out.println("getException().getErrorCode() = " + exception.getErrorCode());
+        throw exception;
+    }
 
+    public static OurCoolUncheckedException getException(int errorCode) {
+        return switch (errorCode) {
+            case 1:
+                yield new OurCoolUncheckedException(
+                        "Наше крутое исключение! Мы получили ошибку: " +
+                                ErrorCodes.FIRST_ERROR.getCode(),
+                        new Throwable(),
+                        ErrorCodes.FIRST_ERROR
+                );
+            case 2:
+                yield new OurCoolUncheckedException(
+                        "Наше крутое исключение! Мы получили ошибку: " +
+                                ErrorCodes.SECOND_CODE.getCode(),
+                        new Throwable(),
+                        ErrorCodes.SECOND_CODE
+                );
+                // здесь мы подхватим тройку и все остальные коды, которые мы еще не добавили
+            default:
+                yield new OurCoolUncheckedException(
+                        "Наше крутое исключение! Мы получили ошибку: " +
+                                ErrorCodes.THIRD_CODE.getCode(),
+                        new Throwable(),
+                        ErrorCodes.THIRD_CODE
+                );
+        };
     }
 }
--- src/Module1/Level21/Tasks/TaskHuman/Solution.java
+++ src/Module1/Level21/Tasks/TaskHuman/Solution.java
@@ -7,6 +7,6 @@ public class Solution {
     public static String message = "Похоже, что у объекта класса Human не инициализирован список детей.";
 
     public static void main(String[] args) {
-        Human amigo = new Human("Amigo", 25, new ArrayList<>());
+        //Human amigo = new Human("Amigo", 25, new ArrayList<>());
     }
 }
