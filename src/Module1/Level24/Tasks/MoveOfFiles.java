a4d24d344873abfda1b345cc4392f8eb18925b0b
maksim.korystov <maksim.korystov@glowbyteconsulting.com>
2024-06-29 00:04:48 +0300

    ADD module1.Level24 Tasks MoveOfFiles

---
+++ src/Module1/Level24/Tasks/MoveOfFiles.java
@@ -0,0 +1,64 @@
+package Module1.Level24.Tasks;
+
+import java.io.IOException;
+import java.util.Scanner;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.DirectoryStream;
+
+/***** Перемещение файлов *****
+ * Напиши программу, которая будет считывать с клавиатуры пути к двум директориям и перемещать файлы
+ * из одной директории в другую (только файлы, директории игнорируй).
+ * Используй соответствующие методы класса Files: newDirectoryStream(), isRegularFile() или isDirectory(), move().
+ * -----
+ * Требования:
+ * • Программа должна считать из консоли пути к директориям.
+ * • В программе должны быть использованы соответствующие методы класса Files согласно условию.
+ * • Программа должна перемещать файлы из одной директории в другую согласно условию.
+ * • Не используй следующие классы: File, FileInputStream, FileOutputStream, FileReader, FileWriter пакета java.io.
+ */
+
+public class MoveOfFiles {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        Path sourceDirectory = Path.of(scanner.nextLine());
+        Path targetDirectory = Path.of(scanner.nextLine());
+
+        try (DirectoryStream<Path> files = Files.newDirectoryStream(sourceDirectory)) {
+            for (Path path : files) {
+                if (Files.isRegularFile(path)) {
+                    Path resolve = targetDirectory.resolve(path.getFileName());
+                    Files.move(path, resolve);
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
