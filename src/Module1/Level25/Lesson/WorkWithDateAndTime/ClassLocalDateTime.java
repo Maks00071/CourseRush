e09cec895d571edfcf125c099b60d255331a0261
maksim.korystov <maksim.korystov@glowbyteconsulting.com>
2024-07-05 19:01:05 +0300

    ADD module1.Level25 Lesson ClassLocalDateTime

---
+++ src/Module1/Level25/Lesson/WorkWithDateAndTime/ClassLocalDateTime.java
@@ -0,0 +1,10 @@
+package Module1.Level25.Lesson.WorkWithDateAndTime;
+
+import java.time.LocalDateTime;
+
+public class ClassLocalDateTime {
+    public static void main(String[] args) {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println("Локальные дата + время = " + localDateTime);
+    }
+}
