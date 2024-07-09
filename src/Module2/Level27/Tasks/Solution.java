752bc52bdd7e12ad0215384b8d2a16bb74c7418e
Maks Korystov <corystov.maks@yandex.ru>
2024-07-09 23:19:11 +0300

    ADD Module 27 Tasks Cat and Dog

---
+++ src/Module2/Level27/Tasks/Solution.java
@@ -0,0 +1,11 @@
+package Module2.Level27.Tasks;
+
+public class Solution {
+    public static void main(String[] args) {
+        Cat cat = new Cat("Vaska", 5, 7, 10);
+        Dog dog = new Dog("Sharik", 4, 10, 15);
+
+        System.out.println(cat.isDogNear(dog));
+        System.out.println(dog.isCatNear(cat));
+    }
+}
