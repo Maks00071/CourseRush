f3e4b488d38885aac298919bbface5807f7d71ab
maksim.korystov <maksim.korystov@glowbyteconsulting.com>
2024-07-10 20:39:53 +0300

    ADD module1.Level27 Tasks WhaleDescendantCow

---
+++ src/Module2/Level27/Tasks/WhaleDescendantCow.java
@@ -0,0 +1,32 @@
+package Module2.Level27.Tasks;
+
+/***** Кит, коровий потомок *****
+ * Переопредели метод getName в классе Whale(Кит), чтобы программа выводила на экран пустую строку.
+ * -----
+ * Требования:
+ * • Программа должна выводить на экран пустую строку.
+ * • Класс Whale должен наследоваться от класса Cow.
+ * • Класс Whale должен переопределять метод getName().
+ * • Метод main() должен вызывать метод getName() у объекта cow.
+ * • Реализуй метод getName() класса Whale, чтобы программа выводила на экран пустую строку.
+ * • Метод main() должен выводить на экран результат вызова метода getName().
+ */
+
+public class WhaleDescendantCow {
+    public static void main(String[] args) {
+        Cow cow = new Whale();
+        System.out.println(cow.getName());
+    }
+
+    public static class Cow {
+        public String getName() {
+            return "Я - корова";
+        }
+    }
+
+    public static class Whale extends Cow {
+        public String getName() {
+            return "";
+        }
+    }
+}
