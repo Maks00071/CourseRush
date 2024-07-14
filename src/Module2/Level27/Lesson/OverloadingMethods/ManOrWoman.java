db0f7fac17a20a74c659b651d5b474a80bcca4bd
Maks Korystov <corystov.maks@yandex.ru>
2024-07-14 23:04:46 +0300

    ADD Module 27 Task ManOrWoman

---
+++ src/Module2/Level27/Lesson/OverloadingMethods/ManOrWoman.java
@@ -0,0 +1,27 @@
+package Module2.Level27.Lesson.OverloadingMethods;
+
+public class ManOrWoman {
+    public static void main(String[] args) {
+        Man man = new Man();
+        Woman woman = new Woman();
+
+        printName(man);
+        printName(woman);
+    }
+
+    public static void printName(Human human) {
+
+    }
+
+    public static class Human {
+
+    }
+
+    public static class Man extends Human {
+
+    }
+
+    public static class Woman extends Human {
+
+    }
+}
