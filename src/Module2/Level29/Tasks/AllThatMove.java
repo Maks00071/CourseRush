98822ab75e3528545330b37a95555cfa9c1479d8
maksim.korystov <maksim.korystov@glowbyteconsulting.com>
2024-07-25 18:42:25 +0300

    ADD module1.Level28 Tasks AllThatMove

---
+++ src/Module2/Level29/Tasks/AllThatMove.java
@@ -0,0 +1,63 @@
+package Module2.Level29.Tasks;
+
+/**
+ * В этой задаче тебе нужно:
+ * Создать интерфейс CanMove с методом speed.
+ * Сделать так, чтобы speed возвращал значение типа Double и ничего не принимал в качестве аргументов.
+ * Создать и унаследовать интерфейс CanFly от интерфейса CanMove.
+ * Добавить в интерфейс CanFly метод speed.
+ * Убедиться, что speed возвращает значение типа Double и принимает один параметр типа CanFly.
+ * Требования:
+ * • В классе Solution должен быть объявлен интерфейс CanMove.
+ * • В классе Solution должен быть объявлен интерфейс CanFly.
+ * • Интерфейс CanFly должен наследоваться от интерфейса CanMove.
+ * • В интерфейсе CanMove должен быть объявлен метод speed без параметров и с типом возвращаемого значения Double.
+ * • В интерфейсе CanFly должен быть объявлен метод speed c одним аргументом типа CanFly
+ *   и с типом возвращаемого значения Double.
+ */
+
+public class AllThatMove {
+    public static void main(String[] args) {
+
+    }
+
+    interface CanMove {
+        Double speed();
+    }
+
+    interface CanFly extends CanMove {
+        Double speed(CanFly canFly);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
