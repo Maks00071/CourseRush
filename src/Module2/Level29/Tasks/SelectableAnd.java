f49680dae3ba1d28a0d408913c7e5aa86fc0ece2
maksim.korystov <maksim.korystov@glowbyteconsulting.com>
2024-07-25 18:26:36 +0300

    ADD module1.Level28 Tasks SelectableAnd

---
+++ src/Module2/Level29/Tasks/SelectableAnd.java
@@ -0,0 +1,64 @@
+package Module2.Level29.Tasks;
+
+/**
+ * Создай класс Screen и реализуй в нем интерфейсы Selectable и Updatable. Не забудь реализовать методы!
+ * -----
+ * Требования:
+ * • Класс Screen должен реализовывать (implements) интерфейс Selectable.
+ * • Класс Screen должен реализовывать (implements) интерфейс Updatable.
+ * • В классе Screen должны быть реализованы методы интерфейса Selectable.
+ * • В классе Screen должны быть реализованы методы интерфейса Updatable.
+ */
+
+public class SelectableAnd {
+    public static void main(String[] args) {
+
+    }
+
+    interface Selectable {
+        void onSelect();
+    }
+
+    interface Updatable {
+        void refresh();
+    }
+
+    public static class Screen implements Selectable, Updatable {
+
+        public void onSelect() {
+            System.out.println("It was selected!");
+        }
+
+        public void refresh() {
+            System.out.println("It was updated!");
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
