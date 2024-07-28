5f0e171a15dc6fcef4775f2d3b0aab8add7cfc11
Maks Korystov <corystov.maks@yandex.ru>
2024-07-28 20:18:36 +0300

    ADD Module2 Level 29 Tasks CorrectionMistakes

---
+++ src/Module2/Level29/Tasks/CorrectionMistakes.java
@@ -0,0 +1,60 @@
+package Module2.Level29.Tasks;
+
+/***** Исправление ошибок *****
+ * Переделай наследование в классах и интерфейсах так,
+ * чтобы программа компилировалась и продолжала делать то же самое.
+ * Класс Hobby должен реализовывать интерфейсы Desire, Dream.
+ * -----
+ * Требования:
+ * • Интерфейс Desire должен быть реализован в классе Hobby.
+ * • Интерфейс Dream должен быть реализован в классе Hobby.
+ * • Класс Hobby должен содержать статическую переменную INDEX.
+ * • Метод toString в классе Hobby должен инкрементировать
+ *   и возвращать переменную INDEX в виде строки.
+ */
+
+public class CorrectionMistakes {
+    public static void main(String[] args) {
+        System.out.println(Dream.HOBBY.toString());
+        System.out.println(new Hobby().INDEX);
+    }
+
+    interface Desire {
+
+    }
+
+    interface Dream {
+        public static Hobby HOBBY = new Hobby();
+    }
+
+    static class Hobby implements Desire, Dream {
+        static int INDEX = 1;
+
+        @Override
+        public String toString() {
+            INDEX++;
+            return "" + INDEX;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
