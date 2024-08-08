8b1f59fa309c654998567f12ed4a0ed221457373
maksim.korystov <maksim.korystov@glowbyteconsulting.com>
2024-08-08 19:35:30 +0300

    ADD StepicJava ZaurJava MethodOverloading2

---
+++ src/StepikJava/ZaurJava/Lessons/MethodOverloading2.java
@@ -0,0 +1,26 @@
+package StepikJava.ZaurJava.Lessons;
+
+public class MethodOverloading2 {
+
+    public int sum(int i1, int i2) {
+        return i1 + i2;
+    }
+
+    public String sum(String s1, String s2) {
+        return s1 + s2;
+    }
+
+}
+
+
+class TestMethodOverloading2 {
+    public static void main(String[] args) {
+        MethodOverloading2 mO2 = new MethodOverloading2();
+
+        int a = mO2.sum(1, 2);
+        String str = mO2.sum("Hello, ", "my friend!");
+
+        System.out.println(a);
+        System.out.println(str);
+    }
+}
