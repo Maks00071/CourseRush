db397aca9dfcd79a7da4728cf3c8a7056f62f146
Maks Korystov <corystov.maks@yandex.ru>
2024-08-10 23:10:30 +0300

    ADD ZaurStepic OneClass and TwoClass

---
+++ src/StepikJava/ZaurJava/Lessons/TaskMultiTwoNumber/TwoClass.java
@@ -0,0 +1,17 @@
+package StepikJava.ZaurJava.Lessons.TaskMultiTwoNumber;
+
+public class TwoClass {
+    public static void main(String[] args) {
+        OneClass oneClass = new OneClass();
+
+        System.out.println(OneClass.multiPlayThree(2, 3, 4));
+
+        OneClass.divTwo(5, 3);
+
+        System.out.println(oneClass.getAreaCircle(2.50));
+
+        System.out.println(OneClass.getCircumference(2.50));
+
+        oneClass.getInfo(2.50);
+    }
+}
