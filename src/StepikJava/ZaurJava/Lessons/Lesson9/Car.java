c50fdd7457f548ca76d39941b1cb4b2b4841f47a
Maks Korystov <corystov.maks@yandex.ru>
2024-08-11 21:57:48 +0300

    ADD ZaurStepic Car

---
+++ src/StepikJava/ZaurJava/Lessons/Lesson9/Car.java
@@ -0,0 +1,55 @@
+package StepikJava.ZaurJava.Lessons.Lesson9;
+
+public class Car {
+    private String color;
+    String engine;
+    static int count;
+
+    public Car(String color, String engine) {
+        count++;    // кол-во созданных машин
+        this.color = color;
+        this.engine = engine;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getEngine() {
+        return engine;
+    }
+
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
+
+    public void showColor() {
+        System.out.println("The color of cars is " + getColor());
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
