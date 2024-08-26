35b16edc38e84d43adecdb3ec8b73fe4aec506d1
Maks Korystov <corystov.maks@yandex.ru>
2024-08-26 22:38:01 +0300

    ADD ZaurStepic Lesson14 Hours

---
+++ src/StepikJava/ZaurJava/Lesson14/Hours.java
@@ -0,0 +1,49 @@
+package StepikJava.ZaurJava.Lesson14;
+
+public class Hours {
+
+    public static void printFromHourTo(int fromHour, int toHour) {
+        OUTER:
+        for (int hour = fromHour; hour < toHour; hour++) {
+            INNER:
+            for (int minute = 0; minute < 60; minute++) {
+                if (hour - 10 < 0) {
+                    if (minute - 10 < 0) {
+                        System.out.println("0" + hour + ":0" + minute);
+                    } else {
+                        System.out.println("0" + hour + ":" + minute);
+                    }
+                } else {
+                    if (minute - 10 < 0) {
+                        System.out.println(hour + ":0" + minute);
+                    } else {
+                        System.out.println(hour + ":" + minute);
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
