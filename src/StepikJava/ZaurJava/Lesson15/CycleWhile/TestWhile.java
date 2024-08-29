82e93bcc03e501e45e3098d6d8a474f083e84214
maksim.korystov <maksim.korystov@glowbyteconsulting.com>
2024-08-29 15:18:03 +0300

    ADD StepicJava ZaurJava Lesson15 TestWhile

---
+++ src/StepikJava/ZaurJava/Lesson15/CycleWhile/TestWhile.java
@@ -0,0 +1,13 @@
+package StepikJava.ZaurJava.Lesson15.CycleWhile;
+
+public class TestWhile {
+    public static void main(String[] args) {
+        int i = 0;
+
+        while(i < 10) {
+            System.out.println(i);
+            i++;
+        }
+
+    }
+}
