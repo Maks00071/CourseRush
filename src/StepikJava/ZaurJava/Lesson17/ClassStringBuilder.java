85ab8ed82e111b2e830d4577c51f3cc7aee9b95f
maksim.korystov <maksim.korystov@glowbyteconsulting.com>
2024-09-03 19:29:31 +0300

    ADD StepicJava ZaurJava Lesson17 StringBuilder

---
+++ src/StepikJava/ZaurJava/Lesson17/ClassStringBuilder.java
@@ -0,0 +1,12 @@
+package StepikJava.ZaurJava.Lesson17;
+
+public class ClassStringBuilder {
+    public static void main(String[] args) {
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder("Good Day!!!");
+        StringBuilder sb3 = new StringBuilder(50);
+        StringBuilder sb4 = new StringBuilder(sb2);
+
+
+    }
+}
