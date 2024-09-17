727733367a6d9c75b38f8a796c3f4da4dd68a1e6
maksim.korystov <maksim.korystov@glowbyteconsulting.com>
2024-09-17 19:13:03 +0300

    ADD StepicJava ZaurJava Lesson21 ClassArrayList

---
+++ src/StepikJava/ZaurJava/Lesson21/ClassArrayList.java
@@ -0,0 +1,30 @@
+package StepikJava.ZaurJava.Lesson21;
+
+import java.util.ArrayList;
+
+public class ClassArrayList {
+    public static void main(String[] args) {
+        // без ограничителей типов данных ArrayList
+        ArrayList list1 = new ArrayList();
+
+        list1.add("Hello");
+
+        Car car = new Car();
+        list1.add(car);
+
+        Student student = new Student();
+        list1.add(student);
+        list1.add(new StringBuilder("Ok"));
+
+        // с ограничителями типов данных ArrayList
+        ArrayList<String> list2 = new ArrayList<>();
+
+        ArrayList<StringBuilder> list3 = new ArrayList<>();
+
+
+    }
+}
+
+class Car{};
+class Student{};
+
