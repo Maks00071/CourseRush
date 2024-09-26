eedd0c54a0ee5e7267b3f5b9b55177765a2f3032
maksim.korystov <maksim.korystov@glowbyteconsulting.com>
2024-09-26 18:50:42 +0300

    ADD StepicJava ZaurJava Lesson25 Homework Student

---
+++ src/StepikJava/ZaurJava/Lesson25/Homework/TestStudent.java
@@ -0,0 +1,17 @@
+package StepikJava.ZaurJava.Lesson25.Homework;
+
+public class TestStudent {
+    public static void main(String[] args) {
+        Student st1 = new Student(new StringBuilder("Ivan"), 3, 7);
+        Student st2 = new Student(new StringBuilder("Petr"), 2, 8);
+        Student st3 = new Student(new StringBuilder("Mila"), 1, 9);
+
+        st1.showInfo();
+        st2.showInfo();
+        st3.showInfo();
+
+        System.out.println(st1.toString());
+        System.out.println(st2.toString());
+        System.out.println(st3.toString());
+    }
+}
