e54651877e811f86733aff5e237b58e4d45ea0a7
Maks Korystov <corystov.maks@yandex.ru>
2024-09-29 19:54:46 +0300

    ADD ZaurStepic Lesson26 Overriding

---
+++ src/StepikJava/ZaurJava/Lesson26/Overriding/Animal.java
@@ -0,0 +1,8 @@
+package StepikJava.ZaurJava.Lesson26.Overriding;
+
+public class Animal {
+
+    public void getName() {
+        System.out.println("Animal");
+    }
+}
---
+++ src/StepikJava/ZaurJava/Lesson26/Overriding/Mouse.java
@@ -0,0 +1,8 @@
+package StepikJava.ZaurJava.Lesson26.Overriding;
+
+public class Mouse extends Animal {
+
+    public void getName() {
+        System.out.println("Mouse");
+    }
+}
