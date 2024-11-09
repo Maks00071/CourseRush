50491303ccdc7b34bbcdecb7a0675844ef0225f2
Maks Korystov <corystov.maks@yandex.ru>
2024-11-09 19:40:05 +0300

    ADD ZaurStepic Lesson35 ClassSupplierAndConsumer

---
+++ src/StepikJava/ZaurJava/Lesson35/FunctionalInterface/ClassSupplierAndConsumer.java
@@ -0,0 +1,23 @@
+package StepikJava.ZaurJava.Lesson35.FunctionalInterface;
+
+import java.util.ArrayList;
+import java.util.function.Supplier;
+import java.util.function.Consumer;
+
+
+public class ClassSupplierAndConsumer {
+
+    public static ArrayList<Car> createThreeCars(Supplier<Car> carSupplier) {
+        ArrayList<Car> carList = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            carList.add(carSupplier.get());
+        }
+        return carList;
+    }
+
+    public static void changeCar(Car car, Consumer consumer) {
+        consumer.accept(car);
+    }
+
+}
