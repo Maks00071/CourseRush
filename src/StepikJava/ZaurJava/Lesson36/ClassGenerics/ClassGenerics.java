b0275373c5b8fb9614288881c07b71bebfbd0773
Maks Korystov <corystov.maks@yandex.ru>
2024-11-10 11:33:16 +0300

    ADD ZaurStepic Lesson36 ClassGenerics

---
+++ src/StepikJava/ZaurJava/Lesson36/ClassGenerics/ClassGenerics.java
@@ -0,0 +1,20 @@
+package StepikJava.ZaurJava.Lesson36.ClassGenerics;
+
+public class ClassGenerics<T> {
+
+    private T value;
+
+    public ClassGenerics (T value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "[{ " + value + " }]";
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+}
---
+++ src/StepikJava/ZaurJava/Lesson36/ClassGenerics/RunClassGenerics.java
@@ -0,0 +1,12 @@
+package StepikJava.ZaurJava.Lesson36.ClassGenerics;
+
+
+public class RunClassGenerics {
+    public static void main(String[] args) {
+        ClassGenerics<String> cg1 = new ClassGenerics<>("Hello!");
+        System.out.println(cg1.toString()); // "[{ Hello! }]"
+
+        ClassGenerics<Integer> cg2 = new ClassGenerics<>(100);
+        System.out.println(cg2.toString()); // "[{ 100 }]"
+    }
+}
