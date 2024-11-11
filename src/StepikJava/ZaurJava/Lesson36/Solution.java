7813280d4db17c06fcde130fb1ada6cc36b05951
Maks Korystov <corystov.maks@yandex.ru>
2024-11-11 20:00:21 +0300

    CHANGE ZaurStepic Lesson37 Modules

---
+++ src/StepikJava/ZaurJava/Lesson36/Solution.java
@@ -0,0 +1,24 @@
+package StepikJava.ZaurJava.Lesson36;
+
+class Solution {
+    public static String camelCase(String input) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < input.length(); i++) {
+            if (Character.isUpperCase(input.charAt(i))) {
+                sb.append(' ');
+                sb.append(input.charAt(i));
+            } else {
+                sb.append(input.charAt(i));
+            }
+
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Solution.camelCase("camelCasing"));
+    }
+}
+
+
+// String output = input.replaceAll("(\\p{Ll})(\\p{Lu})","$1 $2");
