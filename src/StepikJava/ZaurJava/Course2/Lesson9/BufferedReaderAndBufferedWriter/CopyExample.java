6d01360a923a8e65a80ea484d798d60acd4db8e5
Maks00071 <corystov.maks@yandex.ru>
2025-01-07 21:01:13 +0300

    ADD ZaurJava Course2 Lesson9 Streams BufferedReader and BufferedWriter

---
+++ src/StepikJava/ZaurJava/Course2/Lesson9/BufferedReaderAndBufferedWriter/CopyExample.java
@@ -0,0 +1,24 @@
+package StepikJava.ZaurJava.Course2.Lesson9.BufferedReaderAndBufferedWriter;
+
+import java.io.*;
+
+
+public class CopyExample {
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("FileWriterExp.txt"));
+             BufferedWriter writer = new BufferedWriter(new FileWriter("FileExp2.txt"));
+        ) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                writer.write(line);
+                writer.write('\n');
+            }
+            System.out.println("Done!");
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
