46f2c8adccb0559288800e46909dc91b0d56a93f
Maks00071 <corystov.maks@yandex.ru>
2025-01-08 13:50:52 +0300

    ADD ZaurJava Course2 Lesson9 Streams DataInputStream and DataOutputStream

---
+++ src/StepikJava/ZaurJava/Course2/Lesson9/DataInputStreamAndDataOutputStream/DataStreamExp.java
@@ -0,0 +1,51 @@
+package StepikJava.ZaurJava.Course2.Lesson9.DataInputStreamAndDataOutputStream;
+
+import java.io.*;
+
+
+public class DataStreamExp {
+    public static void main(String[] args) {
+        try (DataOutputStream outputStream = new DataOutputStream(new FileOutputStream("my_test.bin"));
+             DataInputStream inputStream = new DataInputStream(new FileInputStream("my_test.bin"));
+             ) {
+            // запишем примитивы в файл
+            outputStream.writeBoolean(true);
+            outputStream.writeByte(5);
+            outputStream.writeShort(120);
+            outputStream.writeInt(5_000);
+            outputStream.writeLong(1_000_000L);
+            outputStream.writeFloat(3.14F);
+            outputStream.writeDouble(125.254);
+
+            System.out.println("Writing done!");
+
+            // прочитаем информацию из файла
+            System.out.println(inputStream.readBoolean());
+            System.out.println(inputStream.readByte());
+            System.out.println(inputStream.readShort());
+            System.out.println(inputStream.readInt());
+            System.out.println(inputStream.readLong());
+            System.out.println(inputStream.readFloat());
+            System.out.println(inputStream.readDouble());
+
+            System.out.println("Reading done!");
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        /*
+        Writing done!
+        true
+        5
+        120
+        5000
+        1000000
+        3.14
+        125.254
+        Reading done!
+         */
+    }
+}
